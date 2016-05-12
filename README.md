bb24c559f6a8d8cd8c91bc94f5dcd44e8f816bd4
Andrew Hood <andrewhood125@gmail.com>
2016-05-11 21:55:03 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+google-cloud-storage
+====================
+
+A php library for interacting with the [Google Cloud Storage JSON API](https://cloud.google.com/storage/docs/json_api/).
---
+++ src/GoogleCloudStorage.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Andrewhood125;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use Google\Auth\ApplicationDefaultCredentials;
+
+class GoogleCloudStorage
+{
+
+    public $baseUrl = 'https://www.googleapis.com/storage/v1';
+
+    public function __construct()
+    {
+        $this->bucket = getenv('GOOGLE_CLOUD_STORAGE_BUCKET');
+
+        // create middleware
+        $middleware = ApplicationDefaultCredentials::getMiddleware(config('storage.scopes'));
+        $stack = HandlerStack::create();
+        $stack->push($middleware);
+
+        // create the HTTP client
+        $this->client = new Client([
+          'handler' => $stack,
+          'auth' => 'google_auth'  // authorize all requests
+        ]);
+    }
+
+
+    /**
+     * https://cloud.google.com/storage/docs/json_api/v1/objects/get
+     */
+    public function get($object) {
+        return json_decode(
+            $this->client->get(
+                "$this->baseUrl/b/$this->bucket/o/$object"
+            )->getBody()
+        );
+    }
+
+    /**
+     * Like `get` but downloads the object.
+     */
+    public function download($object, $path) {
+        $object = $this->get($object);
+        $this->client->get($object->mediaLink, [
+            'sink' => $path
+        ]);
+    }
+
+    /**
+     * https://cloud.google.com/storage/docs/json_api/v1/objects/list
+     */
+    public function ls($nextPageToken = null) {
+        return json_decode(
+            $this->client->get(
+                "$this->baseUrl/b/$this->bucket/o?pageToken=$nextPageToken"
+            )->getBody()
+        );
+    }
+
+}
